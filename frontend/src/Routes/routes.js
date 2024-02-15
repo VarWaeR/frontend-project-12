9e86c9aa2cf9343e4308192e727b94655dbda98a
Kirill <kirill.kltvkin@yandex.ru>
2024-02-15 17:41:28 +0300

    added routes file

---
+++ frontend/src/Routes/routes.js
@@ -0,0 +1,11 @@
+const apiPath = '/api/v1';
+
+const routes = {
+  loginPath: () => [apiPath, 'login'].join('/'),
+  usersPath: () => [apiPath, 'data'].join('/'),
+  signUpPath: () => [apiPath, 'signup'].join('/'),
+  main: () => '/',
+  login: () => 'login',
+  error: () => '*',
+  signup: () => 'signup',
+};
